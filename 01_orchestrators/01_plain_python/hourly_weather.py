451febfdff5e3587a5f8dc0b6c679efde6e89448
aryansamaria <aryansamaria44@gmail.com>
2025-01-18 11:45:09 +0530

    typer

--- 01_orchestrators/01_plain_python/hourly_weather.py
+++ 01_orchestrators/01_plain_python/hourly_weather.py
@@ -1,26 +1,12 @@
-from datetime import datetime
-from dotenv import load_dotenv
-import os
-from db_operations import connect_to_db, create_table, insert_weather_data
-from utils import fetch_hourly_weather
+import typer
+from weather_utils import fetch_and_store_weather
 
-load_dotenv()
+app = typer.Typer()
 
-API_KEY = os.getenv("API_KEY")
+@app.command()
+def fetch_weather(city: str, date: str):
+    
+    fetch_and_store_weather(city, date)
 
 if __name__ == "__main__":
-    city_name = input("Enter the city name: ")
-    today_date = datetime.now().strftime("%Y-%m-%d")  
-
-    with connect_to_db() as conn:
-        
-        create_table(conn)
-
-        weather_data = fetch_hourly_weather(API_KEY, city_name, today_date)
-
-        if weather_data:
-            
-            insert_weather_data(conn, weather_data)
-            print(f"Weather data for {city_name} has been inserted into the database.")
-        else:
-            print("Failed to fetch weather data.")
+    app()
